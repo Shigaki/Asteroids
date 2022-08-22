5ec8ced38a90b869243a46067f82bf53629da1fa
Leandro Shigaki <leandroshigaki@gmail.com>
2022-08-22 06:57:28 -0300

    remove unused files

--- Source/Asteroids/TeleportInterface.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "TeleportInterface.h"
-
-// Add default functionality here for any ITeleportInterface functions that are not pure virtual.
